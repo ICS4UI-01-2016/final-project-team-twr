072af0e7bb763fac5d5cdce845d60077fc95e2a5
tatad6701 <tatad6701@HRH-IC0027335.wrdsb.ca>
2016-12-20 13:57:14 -0500

    Adding more classes and pictures

---
+++ core/src/com/mygdx/game/states/State.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mygdx.game.states;
+
+/**
+ *
+ * @author tatad6701
+ */
+public class State {
+}
---
+++ core/src/com/mygdx/game/states/StateManager.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mygdx.game.states;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author tatad6701
+ */
+public class StateManager {
+
+    private Stack<states> states;
+
+    public StateManager() {
+        states = new Stack<State>();
+    }
+}
