e96c85d653aef1dcd3e9485816bb645e6b4c553c
brayden <brayden@172.21.3.33>
2016-12-20 14:19:32 -0500

    Creating states

--- core/src/com/mygdx/game/states/State.java
+++ core/src/com/mygdx/game/states/State.java
@@ -4,9 +4,76 @@
  */
 package com.mygdx.game.states;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix4;
+
 /**
  *
  * @author tatad6701
  */
-public class State {
+public abstract class State {
+    
+    // Create the constants for the camera and the state manager
+    private OrthographicCamera cam;
+    private StateManager sm;
+    
+    public State(StateManager sm){
+        // Assign the state manager and create the camera
+        sm = sm;
+        cam = new OrthographicCamera();
+    }
+    
+    // Create all of the abstract methods for a state
+    public abstract void render(SpriteBatch batch);
+    public abstract void update(float DeltaTime);
+    public abstract void handleInput();
+    public abstract void dispose();
+    
+    /**
+     * return the orthographic camera
+     * @return the camera
+     */
+    public OrthographicCamera getOrthographicCamera(){
+        return cam;
+    }
+    
+    /**
+     * return the state manager
+     * @return state manager
+     */
+    public StateManager getStateManager(){
+        return sm;
+    }
+    
+    /**
+     * set the camera's current view
+     * @param width the width of the camera
+     * @param height the heigh of the camera
+     */
+    public void setCameraView(float width, float height){
+        cam.setToOrtho(false, width, height);
+        cam.update();
+    }
+    
+    /**
+     * set the camera's current position
+     * @param x the x value of the camera
+     * @param y the y value of the camera
+     */
+    public void setCameraPosition(float x, float y){
+        cam.position.x = x;
+        cam.position.y = y;
+        cam.update();
+    }
+    
+    /**
+     * return the combined camera
+     * @return the combined camera
+     */
+    public Matrix4 getCombinedCamera(){
+        return cam.combined;
+    }
+    
+    
 }
