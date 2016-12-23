3dbf265bed7d93e02eb374e00410889fa01cf20c
richj0985 <richj0985@HRH-IC0027334.wrdsb.ca>
2016-12-23 13:35:06 -0500

    Look more into car movements, trying to think of a way to make it more efficient

--- core/src/com/mygdx/game/states/RaceState.java
+++ core/src/com/mygdx/game/states/RaceState.java
@@ -47,113 +47,113 @@ public class RaceState extends State {
     }
 
     @Override
-    public void handleInput() {        
-        if(Gdx.input.isKeyPressed(Input.Keys.UP)){
-            int turningSpeed = 0;
-            int forwardSpeed = 0;            
+    public void handleInput() {
+            int xCoordinateMove = 0;
+            int yCoordinateMove = 0;   
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)){        
             
             // Q1
-            if(car.getRotation() == 0){
-                turningSpeed = 0;
-                forwardSpeed = 12;
-            }else if(car.getRotation() > 0 && car.getRotation() <= 18){
-                turningSpeed = -2;
-                forwardSpeed = 8;
-            } else if(car.getRotation() > 18 && car.getRotation() <= 36){
-                turningSpeed = -4;
-                forwardSpeed = 6;
-            } else if(car.getRotation() > 36 && car.getRotation() <= 54){
-                turningSpeed = -6;
-                forwardSpeed = 4;
-            } else if(car.getRotation() > 54 && car.getRotation() <= 72){
-                turningSpeed = -8;
-                forwardSpeed = 2;
-            } else if(car.getRotation() > 72 && car.getRotation() < 90){
-                turningSpeed = -10;
-                forwardSpeed = 0;
+            if(car.getRotation() >= 0 && car.getRotation() <= 15){
+                xCoordinateMove = 0;
+                yCoordinateMove = 12;
+            }else if(car.getRotation() > 15 && car.getRotation() <= 30){
+                xCoordinateMove = -2;
+                yCoordinateMove = 8;
+            } else if(car.getRotation() > 30 && car.getRotation() <= 45){
+                xCoordinateMove = -4;
+                yCoordinateMove = 6;
+            } else if(car.getRotation() > 45 && car.getRotation() <= 60){
+                xCoordinateMove = -6;
+                yCoordinateMove = 4;
+            } else if(car.getRotation() > 60 && car.getRotation() <= 75){
+                xCoordinateMove = -8;
+                yCoordinateMove = 2;
+            } else if(car.getRotation() > 75 && car.getRotation() < 90){
+                xCoordinateMove = -10;
+                yCoordinateMove = 0;
             }
             
             // Q2
-            if(car.getRotation() == 90){
-                turningSpeed = -12;
-                forwardSpeed = 0;
-            }else if(car.getRotation() > 90 && car.getRotation() <= 108){
-                turningSpeed = -10;
-                forwardSpeed = -2;
-            } else if(car.getRotation() > 108 && car.getRotation() <= 126){
-                turningSpeed = -8;
-                forwardSpeed = -4;
-            } else if(car.getRotation() > 126 && car.getRotation() <= 144){
-                turningSpeed = -6;
-                forwardSpeed = -6;
-            } else if(car.getRotation() > 144 && car.getRotation() <= 162){
-                turningSpeed = -4;
-                forwardSpeed = -8;
-            } else if(car.getRotation() > 162 && car.getRotation() <= 180){
-                turningSpeed = -2;
-                forwardSpeed = -10;
+            if(car.getRotation() >= 90 && car.getRotation() <= 105){
+                xCoordinateMove = -12;
+                yCoordinateMove = 0;
+            }else if(car.getRotation() > 105 && car.getRotation() <= 120){
+                xCoordinateMove = -10;
+                yCoordinateMove = -2;
+            } else if(car.getRotation() > 120 && car.getRotation() <= 135){
+                xCoordinateMove = -8;
+                yCoordinateMove = -4;
+            } else if(car.getRotation() > 135 && car.getRotation() <= 150){
+                xCoordinateMove = -6;
+                yCoordinateMove = -6;
+            } else if(car.getRotation() > 150 && car.getRotation() <= 165){
+                xCoordinateMove = -4;
+                yCoordinateMove = -8;
+            } else if(car.getRotation() > 165 && car.getRotation() <= 180){
+                xCoordinateMove = -2;
+                yCoordinateMove = -10;
             }
             
             // Q3
             if(car.getRotation() == 180){
-                turningSpeed = 0;
-                forwardSpeed = -12;
+                xCoordinateMove = 0;
+                yCoordinateMove = -12;
             }else if(car.getRotation() > 180 && car.getRotation() <= 198){
-                turningSpeed = 2;
-                forwardSpeed = -8;
+                xCoordinateMove = 2;
+                yCoordinateMove = -8;
             } else if(car.getRotation() > 198 && car.getRotation() <= 216){
-                turningSpeed = 4;
-                forwardSpeed = -6;
+                xCoordinateMove = 4;
+                yCoordinateMove = -6;
             } else if(car.getRotation() > 216 && car.getRotation() <= 234){
-                turningSpeed = 6;
-                forwardSpeed = -4;
+                xCoordinateMove = 6;
+                yCoordinateMove = -4;
             } else if(car.getRotation() > 234 && car.getRotation() <= 252){
-                turningSpeed = 8;
-                forwardSpeed = -2;
+                xCoordinateMove = 8;
+                yCoordinateMove = -2;
             } else if(car.getRotation() > 252 && car.getRotation() < 270){
-                turningSpeed = 10;
-                forwardSpeed = 0;
+                xCoordinateMove = 10;
+                yCoordinateMove = 0;
             }
             
             // Q4
             if(car.getRotation() == 270){
-                turningSpeed = 12;
-                forwardSpeed = 0;
+                xCoordinateMove = 12;
+                yCoordinateMove = 0;
             }else if(car.getRotation() > 270 && car.getRotation() <= 288){
-                turningSpeed = 10;
-                forwardSpeed = 2;
+                xCoordinateMove = 10;
+                yCoordinateMove = 2;
             } else if(car.getRotation() > 288 && car.getRotation() <= 306){
-                turningSpeed = 8;
-                forwardSpeed = 4;
+                xCoordinateMove = 8;
+                yCoordinateMove = 4;
             } else if(car.getRotation() > 306 && car.getRotation() <= 324){
-                turningSpeed = 6;
-                forwardSpeed = 6;
+                xCoordinateMove = 6;
+                yCoordinateMove = 6;
             } else if(car.getRotation() > 324 && car.getRotation() <= 342){
-                turningSpeed = 4;
-                forwardSpeed = 8;
+                xCoordinateMove = 4;
+                yCoordinateMove = 8;
             } else if(car.getRotation() > 342 && car.getRotation() <= 360){
-                turningSpeed = 2;
-                forwardSpeed = 10;
+                xCoordinateMove = 2;
+                yCoordinateMove = 10;
             }
             
-            System.out.println(turningSpeed);
-            System.out.println(forwardSpeed);
+            System.out.println(xCoordinateMove);
+            System.out.println(yCoordinateMove);
             System.out.println(car.getRotation());
             
-                car.moveHorizontal(turningSpeed);
-                car.moveVertical(forwardSpeed);
-        }
-        
-        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
-            car.moveVertical(0);
-        }
-        
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-            car.rotate(4);
-        }
-        
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-            car.rotate(-4);
+                car.moveHorizontal(xCoordinateMove);
+                car.moveVertical(yCoordinateMove);
+                
+            if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+                car.moveVertical(0);
+            }
+
+            if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+                car.rotate(4);
+            }
+
+            if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+                car.rotate(-4);
+            }
         }
     }
 
