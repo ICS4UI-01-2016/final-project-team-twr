ffa369449d3ecc0ede1c9547ab19cde2b4d151cf
Jon <Jon@Jon-HP>
2016-12-23 21:31:33 -0500

    Found a more efficient way of driving, however not sure if it is complete proper format yet

--- core/src/com/mygdx/game/Car.java
+++ core/src/com/mygdx/game/Car.java
@@ -21,10 +21,6 @@ public class Car {
     private Vector3 velocity;
     private TextureRegion carPic;
     private Rectangle bounds;
-    private int moveHorizontal;
-    private int moveVertical;
-//    private final float HORIZONTAL = 100;
-//    private final float VERTICAL = 100;
     
     public Car(int x, int y){
         position = new Vector3(x,y,0);
@@ -34,20 +30,11 @@ public class Car {
     }
     
     public void update(float deltaTime){
-//        // add gravity
-//        velocity.y += HORIZONTAL;
-//        // scaling velocity by time
-//        velocity.scl(deltaTime);
-//        // adding velocity to position
-//        position.add(velocity);
-//        // unscale velocity
-//        velocity.scl(1/deltaTime);
-        
         bounds.setPosition(position.x, position.y);
     }
     
     public void render(SpriteBatch batch){
-        batch.draw(carPic, position.x + moveHorizontal, position.y + moveVertical, carPic.getRegionWidth() / 5, carPic.getRegionHeight() / 5, carPic.getRegionWidth() / 3, carPic.getRegionHeight() / 3, 1, 1, rotation);
+        batch.draw(carPic, position.x, position.y, carPic.getRegionWidth() / 5, carPic.getRegionHeight() / 5, carPic.getRegionWidth() / 4, carPic.getRegionHeight() / 4, 1, 1, rotation);
     }
     
     public float getX(){
@@ -66,16 +53,8 @@ public class Car {
         carPic.getTexture().dispose();
     }
     
-    public void rotate(float degree){
+    public void turn (float degree){
         rotation += degree;
-    }
-    
-    public void moveHorizontal(int speed){
-        moveHorizontal += speed;
-    }
-    
-    public void moveVertical(int speed){
-        moveVertical += speed;
     }
     
     public float getRotation(){
@@ -87,6 +66,11 @@ public class Car {
             rotation = rotation + 360;
         }
         return rotation;
+    }
+    
+    public void drive(float xSpeed, float ySpeed){
+        position.x += xSpeed;
+        position.y += ySpeed;
     }
     
 }
--- core/src/com/mygdx/game/states/RaceState.java
+++ core/src/com/mygdx/game/states/RaceState.java
@@ -43,120 +43,55 @@ public class RaceState extends State {
 
     @Override
     public void update(float deltaTime) {
-//        car.update(deltaTime);
+        car.update(deltaTime);
     }
 
     @Override
     public void handleInput() {
-            int xCoordinateMove = 0;
-            int yCoordinateMove = 0;   
+        float xCoordinateSpeed = 0;
+        float yCoordinateSpeed = 0;
+        float degreeTurned = 0;
+        
+        if(car.getRotation() >= 0 && car.getRotation() <= 90){
+            float rotation = car.getRotation();
+            xCoordinateSpeed = 0 - (rotation / 9);
+            yCoordinateSpeed = 10 - (rotation / 9); 
+        } else if (car.getRotation() >= 90 && car.getRotation() <= 180){
+            float rotation = car.getRotation() - 90;
+            xCoordinateSpeed = -10 + (rotation / 9);
+            yCoordinateSpeed = 0 - (rotation / 9); 
+        } else if (car.getRotation() >= 180 && car.getRotation() <= 270){
+            float rotation = car.getRotation() - 180;
+            xCoordinateSpeed = 0 + (rotation / 9);
+            yCoordinateSpeed = -10 + (rotation / 9); 
+        } else if (car.getRotation() >= 270 && car.getRotation() <= 360){
+            float rotation = car.getRotation() - 270;
+            xCoordinateSpeed = 10 - (rotation / 9);
+            yCoordinateSpeed = 0 + (rotation / 9); 
+        }
+
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            degreeTurned = 4;
+        }
+
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            degreeTurned = -4;
+        }
+        
         if(Gdx.input.isKeyPressed(Input.Keys.UP)){        
-            
-            // Q1
-            if(car.getRotation() >= 0 && car.getRotation() <= 15){
-                xCoordinateMove = 0;
-                yCoordinateMove = 12;
-            }else if(car.getRotation() > 15 && car.getRotation() <= 30){
-                xCoordinateMove = -2;
-                yCoordinateMove = 8;
-            } else if(car.getRotation() > 30 && car.getRotation() <= 45){
-                xCoordinateMove = -4;
-                yCoordinateMove = 6;
-            } else if(car.getRotation() > 45 && car.getRotation() <= 60){
-                xCoordinateMove = -6;
-                yCoordinateMove = 4;
-            } else if(car.getRotation() > 60 && car.getRotation() <= 75){
-                xCoordinateMove = -8;
-                yCoordinateMove = 2;
-            } else if(car.getRotation() > 75 && car.getRotation() < 90){
-                xCoordinateMove = -10;
-                yCoordinateMove = 0;
-            }
-            
-            // Q2
-            if(car.getRotation() >= 90 && car.getRotation() <= 105){
-                xCoordinateMove = -12;
-                yCoordinateMove = 0;
-            }else if(car.getRotation() > 105 && car.getRotation() <= 120){
-                xCoordinateMove = -10;
-                yCoordinateMove = -2;
-            } else if(car.getRotation() > 120 && car.getRotation() <= 135){
-                xCoordinateMove = -8;
-                yCoordinateMove = -4;
-            } else if(car.getRotation() > 135 && car.getRotation() <= 150){
-                xCoordinateMove = -6;
-                yCoordinateMove = -6;
-            } else if(car.getRotation() > 150 && car.getRotation() <= 165){
-                xCoordinateMove = -4;
-                yCoordinateMove = -8;
-            } else if(car.getRotation() > 165 && car.getRotation() <= 180){
-                xCoordinateMove = -2;
-                yCoordinateMove = -10;
-            }
-            
-            // Q3
-            if(car.getRotation() == 180){
-                xCoordinateMove = 0;
-                yCoordinateMove = -12;
-            }else if(car.getRotation() > 180 && car.getRotation() <= 198){
-                xCoordinateMove = 2;
-                yCoordinateMove = -8;
-            } else if(car.getRotation() > 198 && car.getRotation() <= 216){
-                xCoordinateMove = 4;
-                yCoordinateMove = -6;
-            } else if(car.getRotation() > 216 && car.getRotation() <= 234){
-                xCoordinateMove = 6;
-                yCoordinateMove = -4;
-            } else if(car.getRotation() > 234 && car.getRotation() <= 252){
-                xCoordinateMove = 8;
-                yCoordinateMove = -2;
-            } else if(car.getRotation() > 252 && car.getRotation() < 270){
-                xCoordinateMove = 10;
-                yCoordinateMove = 0;
-            }
-            
-            // Q4
-            if(car.getRotation() == 270){
-                xCoordinateMove = 12;
-                yCoordinateMove = 0;
-            }else if(car.getRotation() > 270 && car.getRotation() <= 288){
-                xCoordinateMove = 10;
-                yCoordinateMove = 2;
-            } else if(car.getRotation() > 288 && car.getRotation() <= 306){
-                xCoordinateMove = 8;
-                yCoordinateMove = 4;
-            } else if(car.getRotation() > 306 && car.getRotation() <= 324){
-                xCoordinateMove = 6;
-                yCoordinateMove = 6;
-            } else if(car.getRotation() > 324 && car.getRotation() <= 342){
-                xCoordinateMove = 4;
-                yCoordinateMove = 8;
-            } else if(car.getRotation() > 342 && car.getRotation() <= 360){
-                xCoordinateMove = 2;
-                yCoordinateMove = 10;
-            }
-            
-            System.out.println(xCoordinateMove);
-            System.out.println(yCoordinateMove);
-            System.out.println(car.getRotation());
-            
-                car.moveHorizontal(xCoordinateMove);
-                car.moveVertical(yCoordinateMove);
-                
-            if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
-                car.moveVertical(0);
-            }
+            car.drive(xCoordinateSpeed, yCoordinateSpeed);
+            car.turn(degreeTurned);
+        }
 
-            if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-                car.rotate(4);
-            }
-
-            if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-                car.rotate(-4);
-            }
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+            xCoordinateSpeed = xCoordinateSpeed * -1;
+            yCoordinateSpeed = yCoordinateSpeed * -1;
+            degreeTurned = degreeTurned * -1;
+            car.drive(xCoordinateSpeed, yCoordinateSpeed);
+            car.turn(degreeTurned);
         }
     }
-
+        
     @Override
     public void dispose() {
         car.dispose();
